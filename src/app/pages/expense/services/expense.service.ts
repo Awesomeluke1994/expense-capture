1fe5af718bc7f6f452f493af28f1b49a61ee76b6
lukeliddell1994 <lukeliddell94@gmail.com>
2021-01-25 15:36:34 +0000

    Fixed how I want the id to increment

--- src/app/pages/expense/services/expense.service.ts
+++ src/app/pages/expense/services/expense.service.ts
@@ -6,6 +6,11 @@ import {ExpenseItemRequest} from "../models/expense-item-request";
 
 @Injectable()
 export class ExpenseService {
+
+  protected static readonly EXPENSE_KEY = 'expenses';
+
+  protected static readonly HIGHEST_ID_KEY = 'highestId';
+
   constructor() {
   }
 
@@ -18,14 +23,12 @@ export class ExpenseService {
     return of(expenseItem)
   }
 
-
-
   public addItem(addItem: ExpenseItemRequest): Observable<number> {
     let expenseItems: ExpenseItem[] = ExpenseService.getExpenseItems();
     if (!expenseItems) {
       expenseItems = [];
     }
-    let nextId = ExpenseService.getNextId();
+    let nextId = ExpenseService.getNextIdAndIncrement();
     expenseItems.push({
       expenseDate: new Date(addItem.expenseDate),
       value: addItem.value,
@@ -45,17 +48,22 @@ export class ExpenseService {
     return of(null)
   }
 
-  private static getNextId(): number {
-    let expenseItems = ExpenseService.getExpenseItems();
-    if(expenseItems.length === 0) {return 1}
-    return Math.max(...expenseItems.map(item => item.id)) + 1;
+  private static getNextIdAndIncrement(): number {
+    let id = parseInt(window.localStorage.getItem(this.HIGHEST_ID_KEY));
+    let newId = id ? id + 1 : 1;
+    this.setId(newId);
+    return newId
+  }
+
+  private static setId(nextId: number): void {
+    window.localStorage.setItem(this.HIGHEST_ID_KEY, nextId.toString())
   }
 
   private static getExpenseItems(): ExpenseItem[] {
-    return JSON.parse(window.localStorage.getItem('expenses'));
+    return JSON.parse(window.localStorage.getItem(this.EXPENSE_KEY));
   }
 
   private static storeExpenses(expenseItems: ExpenseItem[]) {
-    window.localStorage.setItem('expenses', JSON.stringify(expenseItems));
+    window.localStorage.setItem(this.EXPENSE_KEY, JSON.stringify(expenseItems));
   }
 }
